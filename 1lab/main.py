20029726db89f0786a667ef9b212f78bb3d777a7
Kuzne41ck <edrenbaton84@gmail.com>
2025-09-06 00:28:19 +0300

    Update main.py

--- 1lab/main.py
+++ 1lab/main.py
@@ -1,4 +1,21 @@
-
-
-if __name__ == "__main__":
-    pass # Ваш код здесь
+import random
+students = int(input('Введите количество учеников '))
+subjects = int(input('Введите количество предметов '))
+a = [[x for x in range(subjects)] for y in range(students)]
+for i in range(students):
+    for j in range(subjects):
+        a[i][j]=random.randint(1, 5)
+best = -100
+worst = 100
+for i in range(len(a)):
+    average_mark=sum(a[i])/len(a[i])
+    max_mark, min_mark=max(a[i]), min(a[i])
+    if average_mark>best:
+        best = average_mark
+        besti = i
+    if average_mark<worst:
+        worst = average_mark
+        worsti = i
+    print(i+1, a[i], average_mark, max_mark, min_mark)
+print('Студент с лучшим средним баллом под номером ', besti+1)
+print('Студент с худшим средним баллом под номером ', worsti+1)
