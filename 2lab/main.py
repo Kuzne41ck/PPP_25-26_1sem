6dc3594346530759d7f74bda6c75d237436b7e0b
Kuzne41ck <edrenbaton84@gmail.com>
2025-10-05 18:48:02 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,4 +1,14 @@
-
-
-if __name__ == "__main__":
-    pass # Ваш код здесь
+import string
+stroka = "aCaaCAcAbcbBCBB"
+print(stroka)
+l = len(stroka)
+try:
+    for i in range(l): 
+        a = stroka[i]
+        c = stroka.count(a)
+        if 97<=ord(a)<=122:
+            if c>stroka.count(chr(ord(a)-32)):
+                stroka = stroka.replace(a, '')
+                stroka = stroka.replace(chr(ord(a)-32), '')
+except IndexError:
+    print(stroka)
